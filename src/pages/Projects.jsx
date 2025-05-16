65382ef9e2b2f58b1acffbe9a00e046f691fbc1c
Harun Işık <157045502+harunidev@users.noreply.github.com>
2025-05-17 00:36:05 +0300

    Little Update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
       description: "A flight reservation platform that allows users to view available routes, select flights, and purchase tickets via a web-based interface.",
       technologies: ["ASP.NET MVC", "HTML", "CSS", "JavaScript"],
       image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80",
-      link: "https://github.com/harunidev/AirlineReservationSystem-with-ASPNETn"
+      link: "https://github.com/harunidev/AirlineReservationSystem-with-ASPNET"
     },
     {
       title: "Weather App with API Integration",
